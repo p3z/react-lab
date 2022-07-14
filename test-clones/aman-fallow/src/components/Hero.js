b4b6dd4999a1f2dbe0f7b3bef6b77d5960643629
Linux PC (Shay) <shay@pottleweb.solutions>
2022-07-14 23:21:16 +0100

    Build and style the hero

--- test-clones/aman-fallow/src/components/Hero.js
+++ test-clones/aman-fallow/src/components/Hero.js
@@ -4,6 +4,13 @@ function Hero() {
   return (    
       <div className = "app-hero">
 
+          <div className = "app-hero__container">
+            <h2 className = "app-hero__heading">Aman Fallow</h2>
+            <p className = "app-hero__tagline">A man behind a mask</p>          
+            <a href = "#about" className = "app-hero__cta">His story</a>
+          </div>
+          
+
       </div>
   );
 }
